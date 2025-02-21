a215ee09e73d57206260df78bf9742d48f4be065
Rahul Nimbalkar <156823477+rjnimbalkar99@users.noreply.github.com>
2025-02-21 22:29:02 +0530

    Update tools-install.sh

--- tools-install.sh
+++ tools-install.sh
@@ -11,7 +11,7 @@ java --version
 #!/bin/bash
 sudo apt update
 sudo apt install docker.io -y
-sudo usermod -aG docker jenkins
+sudo usermod -aG docker azureuser
 sudo usermod -aG docker ubuntu
 sudo systemctl restart docker
 sudo chmod 777 /var/run/docker.sock
